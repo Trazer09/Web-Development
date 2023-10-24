99410f4d1d7a4ba804fc23c957e48b0c5aeaa2eb
Sidhant <84363346+Trazer09@users.noreply.github.com>
2023-10-24 15:34:31 +0530

    Added GSAP

--- Projects/Golf Ad/script.js
+++ Projects/Golf Ad/script.js
@@ -1,1 +1,15 @@
- 
+gsap.to("#navbar", {
+    backgroundColor: "black",
+    height: "110px",
+
+    duration: 0.5,
+    scrollTrigger:{
+        trigger: "#navbar",
+        scroller: "body",
+        markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub:1
+    }
+    
+})
