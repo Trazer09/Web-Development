f228c753710d874c2cf1ae45c5a14e129d2e5d15
Sidhant Kumar <84363346+Trazer09@users.noreply.github.com>
2025-03-28 16:24:23 +0530

    added flowbite and register.ejs

--- Projects/Google Drive Project/app.js
+++ Projects/Google Drive Project/app.js
@@ -1,14 +1,14 @@
 const express = require ('express')
+const userRouter = require('./routes/user.routes')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res)=>{
-    res.render("index")
-})
-
+app.use('/user', userRouter)
 
 app.listen(3000, ()=>{
     console.log("Server Running Successfully!")
