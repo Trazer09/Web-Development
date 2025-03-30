6cdde35bed859885867b724dd4ae3e3c586d7d17
Sidhant Kumar <84363346+Trazer09@users.noreply.github.com>
2025-03-30 10:53:56 +0530

    added home.ejs and index.routes.js

--- Projects/Google Drive Project/app.js
+++ Projects/Google Drive Project/app.js
@@ -1,7 +1,8 @@
 
-
 const express = require ('express')
 const userRouter = require('./routes/user.routes')
+
+const indexRouter = require('./routes/index.routes')
 
 const app = express()
 const connectToDB = require('./config/db')
@@ -9,6 +10,8 @@ const connectToDB = require('./config/db')
 const dotenv = require ('dotenv');
 dotenv.config();  // calling the config folder
 
+
+app.use('/', indexRouter)        // by default the slash route (/) will act as the indexRouter
 
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
